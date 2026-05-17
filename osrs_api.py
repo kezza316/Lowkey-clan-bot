793772a952b8c01bd6ab030b4b80bbc709c69cbd
kezza316 <k.syrett06@gmail.com>
2026-05-17 16:22:20 +0100

    Update osrs_api.py

--- osrs_api.py
+++ osrs_api.py
@@ -4,10 +4,12 @@ HISCORES_URL = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?p
 
 
 async def get_total_level(rsn: str):
+
     url = HISCORES_URL + rsn.replace(" ", "%20")
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as r:
+
             if r.status != 200:
                 return None
 
